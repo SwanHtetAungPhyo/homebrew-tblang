482fa4130c069d2de9309d642f147a6cac73a328
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2025-11-13 23:23:10 +0100

    Initial commit: Add TBLang formula

---
+++ Formula/tblang.rb
@@ -0,0 +1,68 @@
+class Tblang < Formula
+  desc "Plugin-based Infrastructure as Code language"
+  homepage "https://github.com/SwanHtetAungPhyo/tblang"
+  url "https://github.com/SwanHtetAungPhyo/tblang.git",
+      branch: "main"
+  license "MIT"
+  version "0.1.0"
+  head "https://github.com/SwanHtetAungPhyo/tblang.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    if (buildpath/"core").exist?
+      cd "core" do
+        system "go", "build", "-ldflags", "-s -w", "-o", "tblang", "./cmd/tblang"
+        bin.install "tblang"
+      end
+
+      cd "plugin/aws" do
+        system "go", "build", "-o", "tblang-provider-aws", "main.go"
+        (lib/"tblang/plugins").install "tblang-provider-aws"
+      end
+
+      if (buildpath/"tblang-demo").exist?
+        (prefix/"examples").install Dir["tblang-demo/*.tbl"]
+        (prefix/"examples").install Dir["tblang-demo/*.md"]
+      end
+    else
+      odie "Repository structure not found. Please check the installation."
+    end
+  end
+
+  def post_install
+    (var/"tblang").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      TBLang has been installed!
+
+      The AWS provider plugin is installed at:
+        #{lib}/tblang/plugins/tblang-provider-aws
+
+      Example files are available at:
+        #{prefix}/examples/
+
+      To get started:
+        1. Configure your AWS credentials:
+           aws configure --profile your-profile
+
+        2. Create a .tbl file (see examples)
+
+        3. Run TBLang commands:
+           tblang plan infrastructure.tbl
+           tblang apply infrastructure.tbl
+           tblang show
+           tblang destroy infrastructure.tbl
+
+      For more information:
+        tblang --help
+    EOS
+  end
+
+  test do
+    system "#{bin}/tblang", "--version"
+    system "#{bin}/tblang", "plugins", "list"
+  end
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# Homebrew TBLang
+
+Homebrew tap for [TBLang](https://github.com/SwanHtetAungPhyo/tblang) - Plugin-based Infrastructure as Code language.
+
+## Installation
+
+```bash
+brew tap swanhtetaungphyo/tblang
+brew install tblang
+```
+
+## Usage
+
+```bash
+tblang --version
+tblang plugins list
+tblang plan infrastructure.tbl
+tblang apply infrastructure.tbl
+```
+
+## Documentation
+
+See the main [TBLang repository](https://github.com/SwanHtetAungPhyo/tblang) for full documentation.
